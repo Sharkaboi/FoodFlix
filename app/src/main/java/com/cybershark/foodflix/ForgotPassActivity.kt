365684992d0ab4c38cf7529603a5deb8d4dfa055
Sharkaboi <cybersharbusiness@gmail.com>
2020-03-19 22:44:40 +0530

    Initial commit

---
+++ app/src/main/java/com/cybershark/foodflix/ForgotPassActivity.kt
@@ -0,0 +1,12 @@
+package com.cybershark.foodflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForgotPassActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_pass)
+    }
+}
---
+++ app/src/main/java/com/cybershark/foodflix/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.cybershark.foodflix
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //directly loads to Next Activity thus not wasting user's time and taking exactly as much time as the phone take's to load the next activity.
+        startActivity(Intent(this,LoginActivity::class.java))
+        finish()
+    }
+}
